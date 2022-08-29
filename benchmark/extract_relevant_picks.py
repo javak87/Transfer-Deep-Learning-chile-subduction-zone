b54ba8b030402c9cba1bfec2b174172329b25200
javak87 <javak87@mi.fu-berlin.de>
2022-08-29 14:02:28 +0200

    add documentation

--- benchmark/extract_relevant_picks.py
+++ benchmark/extract_relevant_picks.py
@@ -1,51 +1,60 @@
-import obspy
 import os
 import pickle
-import datetime
-import seisbench
-import numpy as np
 import pandas as pd
-from scipy.spatial import distance_matrix
 import matplotlib.pyplot as plt
-from picks_comparison import Picks_Comparison
 import warnings
 warnings.filterwarnings('ignore')
 
-
-
 class extract_relevant_picks (object):
 
-    def __init__ (self, files_address):
+    def __init__ (self, files_address:'str'):
+
+        '''
+        This class goes through pickle files and select the picks files among different files.
+        Then, return those picks which are associated.
+
+            - files address: the directory of tunning files 
+        '''
 
         self.files_address = files_address
 
         tuning_files = os.listdir(self.files_address)
 
+        # select the catalog_files
         self.catalog_files = list(filter(lambda k: 'catalog_' in k, tuning_files))
         self.catalog_files_compare = [elem.replace('catalog_', '') for elem in self.catalog_files]
-
+        
+        # select the assignment_files
         self.assignment_files = list(filter(lambda k: 'assignments_' in k, tuning_files))
         self.assignment_files_compare  = [elem.replace('assignments_', '') for elem in self.assignment_files]
 
-    def extract_picks (self,picks_file_name):
+    def extract_picks (self,picks_file_name:'str') -> pd.DataFrame:
 
+        # select files contains "picks"
         picks_files_compare = picks_file_name.replace('picks_', '')
 
+        # select the relevant catalog files realted the the picks file
         catalog_file = self.catalog_files[self.catalog_files_compare.index(picks_files_compare)]
 
+        # select the relevant assignment files realted the the picks file
         assignments_file = self.assignment_files [self.assignment_files_compare.index(picks_files_compare)]
 
+        # read the relevant catalog file
         with open(os.path.join(self.files_address, catalog_file),'rb') as fp:
             catalog = pickle.load(fp)
         
+        # read the relevant catalog file
         with open(os.path.join(self.files_address, assignments_file),'rb') as fe:
             assignments = pickle.load(fe)
         
+        # read the relevant picks file
         with open(os.path.join(self.files_address, picks_file_name),'rb') as fs:
             picks = pickle.load(fs)
         
+        # create a data frame
         selected_picks = pd.DataFrame(columns = ['id', 'timestamp', 'prob', 'type'])
 
+        # Extract those picks which are existed in Catalog
         for event_idx in catalog.event_idx.index:
 
             event_picks = picks.iloc[assignments[assignments["event_idx"] == event_idx]["pick_idx"]]
@@ -53,10 +62,6 @@ class extract_relevant_picks (object):
         
         return selected_picks
 
-
-
-
-    
 if __name__ == "__main__":
 
     tuning_files_add = '/home/javak/.seisbench/datasets/chile/parameters_tunning'
